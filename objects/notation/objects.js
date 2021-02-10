bed90904a8da98b49ee8873cd332c11ada7a3526
Josh Archer <jarcher@greenriver.edu>
2021-02-10 11:19:43 -0800

    Added several examples of arrays and objects.

--- objects/notation/objects.js
+++ objects/notation/objects.js
@@ -5,8 +5,13 @@ let ourClass = {
     seats: 20,
     courseNumber: "SDEV 117",
     students: [
-        "Yuki", "Thomas", "Patrick", "Mayank", "Mathieu",
-        "Lennox", "Kevin"
+        {first: "Yuki", last: "Yamada"},
+        {first: "Thomas", last: "Loudon"},
+        {first: "Patrick", last: "Luong"},
+        {first: "Mayank", last: "Parkash"},
+        {first: "Mathieu", last: "Cameron"},
+        {first: "Lennox", last: "???"},
+        {first: "Kevin", last: "Price"}
     ],
     building: "Home",
     roomNumber: 1,
@@ -18,8 +23,31 @@ let ourClass = {
     },
     virtual: true,
     tutorsAvailable: true,
-    projectorAvailable: false
+    projectorAvailable: false,
+    incrementSeats: function() {
+        //this function is located in an object!
+        //the keyword "this" represents the object
+        //that contains this function
+        this.seats++;
+    },
+    printInstructor: function() {
+        console.log(this.instructor.first + " " +
+                    this.instructor.last);
+    },
+    changeRoomNumber: changeNumber
 };
+
+let storeInMall = {
+    name: "Verizon Store",
+    owner: "Bob Smith",
+    roomNumber: 11,
+    changeRoomNumber: changeNumber
+}
+
+function changeNumber(number)
+{
+    this.roomNumber = number;
+}
 
 //interact with some interesting properties on our class
 console.log(ourClass.title);
@@ -33,7 +61,7 @@ console.log(ourClass); //log the entire object (very important)!
 console.log();
 for (let i = 0; i < ourClass.students.length; i++)
 {
-    console.log(ourClass.students[i]);
+    console.log(ourClass.students[i].first);
 }
 console.log();
 
@@ -49,8 +77,16 @@ else
 //print out nested object properties
 console.log("Office hours: " + ourClass.instructor.officeHours)
 
+console.log("Seats: " + ourClass.seats);
+ourClass.incrementSeats();
+ourClass.incrementSeats();
+ourClass.incrementSeats();
+console.log("Seats: " + ourClass.seats);
+console.log();
 
-
+ourClass.printInstructor();
+ourClass.changeRoomNumber(3);
+console.log("Room #" + ourClass.roomNumber);
 
 
 
