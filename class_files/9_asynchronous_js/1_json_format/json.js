1f1e253fda3578c7a68b049886a8c8b35b635d29
Josh Archer <jarcher@greenriver.edu>
2021-03-05 09:56:39 -0800

    Added an example web page connecting to a remote web API.

--- class_files/9_asynchronous_js/1_json_format/json.js
+++ class_files/9_asynchronous_js/1_json_format/json.js
@@ -1,2 +1,13 @@
 let object =  {name: "John", age: 31, city: "New York"};
 let jsonText =  '{"name": "John", "age": 31, "city": "New York"}';
+
+//print out the object and JSON string to the console
+console.log(object);
+console.log(jsonText);
+
+//convert the text to an object
+let person = JSON.parse(jsonText);
+console.log(person);
+
+let text = JSON.stringify(object);
+console.log(text);
--- class_files/9_asynchronous_js/2_web_apis/script.js
+++ class_files/9_asynchronous_js/2_web_apis/script.js
@@ -2,6 +2,30 @@ window.onload = function() {
     let button = document.querySelector("button");
 
     //add an event handler...
+    button.onclick = loadDogPicture;
 }
 
+function loadDogPicture()
+{
+    //the fetch() call requires an address (URL) and
+    //an object with connection properties (options)
+    fetch("https://api.thedogapi.com/v1/images/search", {
+        method: "GET",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+            "x-api-key": "2ef377a9-d455-487a-9aeb-cff4084eb779"
+        }
+    })
+    .then(function(response) { //this function is called, when fetch() completes
+        return response.json();
+    })
+    .then(function(json) {
+        console.log(json);
 
+        //show the dog image
+        let url = json[0].url;
+        let image = document.querySelector("img");
+        image.setAttribute("src", url);
+    });
+}
