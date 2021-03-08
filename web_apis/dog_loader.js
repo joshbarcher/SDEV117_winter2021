ad1ee92308d0996d89b51b4a46a9292aa031023c
Josh Archer <jarcher@greenriver.edu>
2021-03-08 10:39:35 -0800

    Updated our breeds example to load extra information about breeds.

--- web_apis/dog_loader.js
+++ web_apis/dog_loader.js
@@ -1,7 +1,7 @@
 //load the breed images on page load
 window.onload = function() {
     //request breeds, load them in the function below
-    fetch("https://api.thedogapi.com/v1/breeds", {
+    fetch("https://api.thedogapi.com/v1/breeds?limit=10&page=3", {
         method: "GET",
         mode: "cors",
         headers: {
@@ -26,16 +26,22 @@ function loadBreedImages(data)
         //create a section element with nested image and paragraph
         let section = document.createElement("section");
         let image = document.createElement("img");
-        let paragraph = document.createElement("p");
+        let breedName = document.createElement("p");
+        let bredFor = document.createElement("p");
+        let temperament = document.createElement("p");
 
         //create content in elements
         image.setAttribute("src", breed.image.url);
         image.setAttribute("alt", "A dog picture");
-        paragraph.textContent = breed.name;
+        breedName.textContent = breed.name;
+        bredFor.textContent = "Bred for: " + breed.bred_for;
+        temperament.textContent = "Temperament: " + breed.temperament;
 
         //assemble in the dom
         section.appendChild(image);
-        section.appendChild(paragraph);
+        section.appendChild(breedName);
+        section.appendChild(bredFor);
+        section.appendChild(temperament);
         document.body.appendChild(section);
     }
 }
